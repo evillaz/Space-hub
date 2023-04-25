b5021d18de1f19a80e669228f34b4b4fdb0cd01e
evillaz <evillaz123@gmail.com>
2023-04-25 02:02:15 -0500

    Update Repo Setup

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Rockets from './routes/Rockets';
+import Missions from './routes/Missions';
+import Profile from './routes/MyProfile';
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="profile" element={<Profile />} />
+          <Route path="missions" element={<Missions />} />
+          <Route path="/" element={<Rockets />} />
+        </Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
